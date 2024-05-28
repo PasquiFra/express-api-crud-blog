1299a21b07c5640c90c85b873fa53366134329ef
Your Name <you@example.com>
2024-05-28 15:20:24 +0200

    set home '/' route

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import del modulo path
+const path = require("path");
 
 // Setup server with express
 const express = require("express");
@@ -5,7 +7,14 @@ const app = express();
 const port = 3000;
 
 //aggiungo il middleware che si occupa della cartella public
-app.use(express.static('public'));
+//app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Imposto la rotta Home e restituisco una vista standard
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, './index.html');
+    res.sendFile(filePath);
+})
 
 app.listen(port, () => {
     console.log(`Server avviato alla porta http://localhost:${port}.`);
