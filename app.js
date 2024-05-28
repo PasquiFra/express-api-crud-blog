ba2be6ff7ff4ccedfbccb8210a4cc50e0cf52940
Your Name <you@example.com>
2024-05-28 14:48:45 +0200

    added public folder, added imgs, added posts data from previous ex.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 //aggiungo il middleware che si occupa della cartella public
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server avviato alla porta http://localhost:${port}.`);
